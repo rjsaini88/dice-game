8e4fd43c9e46e965db57ce93dc49bb9d8fccef28
Rj Saini <rsaini88@gmail.com>
2023-07-24 16:36:00 -0700

    added dice function

--- script.js
+++ script.js
@@ -3,6 +3,9 @@
 //Selecting elements
 const score0El = document.getElementById('score--0');
 const score1El = document.querySelector('#score--1');
+const current0El = document.getElementById('current--0');
+const current1El = document.getElementById('current--1');
+
 const diceEl = document.querySelector('.dice');
 const btnNew = document.querySelector('.btn--new');
 const btnRoll = document.querySelector('.btn--roll');
@@ -12,10 +15,24 @@ score0El.textContent = 0;
 score1El.textContent = 0;
 diceEl.classList.add('hidden');
 
-//Rolling Dice functionality
+const scores = [0, 0];
+let currentScore = 0;
+let activePlayer = 0;
+
+//Rolling Dice functionality to generate random dice roll on click
 btnRoll.addEventListener('click', function () {
   const dice = Math.trunc(Math.random() * 6) + 1;
+  console.log(dice);
+
+  //display dice image based on the number rolled.
 
   diceEl.classList.remove('hidden');
   diceEl.src = `dice-${dice}.png`;
+
+  if (dice !== 1) {
+    currentScore += dice;
+    current0El.textContent = currentScore; //TODO: implement to do the same for player two.
+  } else {
+    //If rolled one, reset score and switch player.
+  }
 });
