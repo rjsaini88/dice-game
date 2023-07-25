562700256fef18ecd5f3a2c84e188871e583b1b4
Rj Saini <rsaini88@gmail.com>
2023-07-24 17:48:34 -0700

    added the new game feature

--- script.js
+++ script.js
@@ -14,13 +14,26 @@ const btnNew = document.querySelector('.btn--new');
 const btnRoll = document.querySelector('.btn--roll');
 const btnHold = document.querySelector('.btn--hold');
 
-score0El.textContent = 0;
-score1El.textContent = 0;
-diceEl.classList.add('hidden');
+let scores, currentScore, activePlayer, playing;
 
-const scores = [0, 0];
-let currentScore = 0;
-let activePlayer = 0;
+const init = function () {
+  scores = [0, 0];
+  currentScore = 0;
+  activePlayer = 0;
+  playing = true;
+
+  score0El.textContent = 0;
+  score1El.textContent = 0;
+  current0El.textContent = 0;
+  current1El.textContent = 0;
+
+  player0El.classList.remove('player-winner');
+  player1El.classList.remove('player-winner');
+  player0El.classList.add('player-active');
+  player1El.classList.remove('player-active');
+  diceEl.classList.add('hidden');
+};
+init();
 
 const switchPlayer = function () {
   document.getElementById(`current--${activePlayer}`).textContent = 0;
@@ -28,30 +41,49 @@ const switchPlayer = function () {
   activePlayer = activePlayer === 0 ? 1 : 0;
   player0El.classList.toggle('player--active');
   player1El.classList.toggle('player--active');
-}
+};
 //Rolling Dice functionality to generate random dice roll on click
 btnRoll.addEventListener('click', function () {
-  const dice = Math.trunc(Math.random() * 6) + 1;
-  console.log(dice);
+  if (playing) {
+    const dice = Math.trunc(Math.random() * 6) + 1;
+    console.log(dice);
 
-  //display dice image based on the number rolled.
+    //display dice image based on the number rolled.
 
-  diceEl.classList.remove('hidden');
-  diceEl.src = `dice-${dice}.png`;
+    diceEl.classList.remove('hidden');
+    diceEl.src = `dice-${dice}.png`;
 
-  if (dice !== 1) {
-    currentScore += dice;
-    document.getElementById(`current--${activePlayer}`).textContent =
-      currentScore;
-    // current0El.textContent = currentScore; //TODO: implement to do the same for player two.
-  } else {
-    //If rolled one, reset score and switch player.
-    switchPlayer ()
+    if (dice !== 1) {
+      currentScore += dice;
+      document.getElementById(`score--${activePlayer}`).textContent =
+        currentScore;
+      // current0El.textContent = currentScore; //TODO: implement to do the same for player two.
+    } else {
+      //If rolled one, reset score and switch player.
+      switchPlayer();
+    }
   }
 });
 
-btnHold.addEventListener('click', function{
-  scores[activePlayer] += currentScore;
-  document.getElementById(`current--${activePlayer}`).textContent = scores[activePlayer];
-  switchPlayer();
-})
+btnHold.addEventListener('click', function () {
+  if (playing) {
+    diceEl.classList.add('hidden');
+
+    scores[activePlayer] += currentScore;
+    document.getElementById(`current--${activePlayer}`).textContent =
+      scores[activePlayer];
+    if (scores[activePlayer] >= 100) {
+      playing = false;
+      document
+        .querySelector(`.player--${activePlayer}`)
+        .classList.add('player--winner');
+      document
+        .querySelector(`.player--${activePlayer}`)
+        .classList.remove('player--active');
+    } else {
+      switchPlayer();
+    }
+  }
+});
+
+btnNew.addEventListener('click', init);
