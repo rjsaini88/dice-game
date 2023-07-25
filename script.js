02d847eb753d033a35439462936776d714e0b8d8
Rj Saini <rsaini88@gmail.com>
2023-07-24 17:20:49 -0700

    added switch player function

--- script.js
+++ script.js
@@ -1,6 +1,9 @@
 'use strict';
 
 //Selecting elements
+const player0El = document.querySelector('.player--0');
+const player1El = document.querySelector('.player--1');
+
 const score0El = document.getElementById('score--0');
 const score1El = document.querySelector('#score--1');
 const current0El = document.getElementById('current--0');
@@ -19,6 +22,13 @@ const scores = [0, 0];
 let currentScore = 0;
 let activePlayer = 0;
 
+const switchPlayer = function () {
+  document.getElementById(`current--${activePlayer}`).textContent = 0;
+  currentScore = 0;
+  activePlayer = activePlayer === 0 ? 1 : 0;
+  player0El.classList.toggle('player--active');
+  player1El.classList.toggle('player--active');
+}
 //Rolling Dice functionality to generate random dice roll on click
 btnRoll.addEventListener('click', function () {
   const dice = Math.trunc(Math.random() * 6) + 1;
@@ -31,8 +41,17 @@ btnRoll.addEventListener('click', function () {
 
   if (dice !== 1) {
     currentScore += dice;
-    current0El.textContent = currentScore; //TODO: implement to do the same for player two.
+    document.getElementById(`current--${activePlayer}`).textContent =
+      currentScore;
+    // current0El.textContent = currentScore; //TODO: implement to do the same for player two.
   } else {
     //If rolled one, reset score and switch player.
+    switchPlayer ()
   }
 });
+
+btnHold.addEventListener('click', function{
+  scores[activePlayer] += currentScore;
+  document.getElementById(`current--${activePlayer}`).textContent = scores[activePlayer];
+  switchPlayer();
+})
